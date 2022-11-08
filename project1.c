d6b82f4d45faec29d168be9a6516c75bfa5a1003
wexsu <85936457+wexsu@users.noreply.github.com>
2022-11-07 22:18:05 -0800

    Update project1.c
    user space

--- project1.c
+++ project1.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <linux/kernel.h>
 #include <sys/syscall.h>
 
-#define __NR_get_address 0
+#define __NR_get_address 436
 
 int global_in = 10; //data segment
 int global_nin; //BSS segment
@@ -16,15 +15,17 @@ void text() //text
     printf( "hello\n");
 }
 
-long get_addresss( void *addr )
+long get_address( void *addr )
 {
-    return syscall( __NR_get_address, addr );
+    return syscall( 700, addr );
 }
 
 void *child( void *arg )
 {
     int local = 123;
     int *id = arg;
+
+
 /*    
     printf( "This is thread %d:\n", *id );
     printf( "[global_in] vaddr: %p, paddr:  --data\n", &global_in );
@@ -36,12 +37,14 @@ void *child( void *arg )
     printf( "\n" );
 */
 
+    sleep(*id);
+
     printf( "This is thread %d:\n", *id );
     printf( "[global_in] vaddr: %p, paddr: %p --data\n", &global_in, (void *)get_address( &global_in ) );
     printf( "[global_nin] vaddr: %p, paddr: %p --BSS\n", &global_nin, (void *)get_address( &global_nin ) );
     printf( "[heap] vaddr: %p, paddr: %p --heap\n", &heap, (void *)get_address( &heap ) );
-    printf( "[libraries] vaddr: %p, paddr: %p  --libraries\n", &libraries, (void *)get_address( &libraries ) );
-    printf( "[text] vaddr: %p, paddr: %p --text\n", &text, (void *)get_addres( &text ) );
+    printf( "[libraries] vaddr: %p, paddr: %p  --libraries\n", &libraries, (void *)get_address( libraries ) );
+    printf( "[text] vaddr: %p, paddr: %p --text\n", &text, (void *)get_address( &text ) );
     printf( "[local] vaddr: %p, paddr: %p --stack\n", &local, (void *)get_address( &local ) );
     printf( "\n" );
 
@@ -57,7 +60,13 @@ int main()
     
     pthread_t id1, id2, id3;
     int id[3] = { 0, 1, 2 };
-    
+    int a=1,b=2;
+    //syscall( 701, 143 );
+    //int r=syscall(702, 12,13 );
+    //printf( "this is b %d\n", r );
+    //printf("%p, %p\n%p, %p\n",&a, get_address( &a ),&global_in, (void *)get_address( &global_in ));
+    //printf("%p, %p\n",&a, (void *)get_address( &a ));
+  
     pthread_create( &id1, NULL, child, &id[0] );
     pthread_create( &id2, NULL, child, &id[1] );
     pthread_create( &id3, NULL, child, &id[2] );
